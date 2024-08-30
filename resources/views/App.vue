ec042a6c577a4603eb29566f71fd514c4e413b06
Newt Louis <l.ngoc.17395@gmail.com>
2024-08-30 23:27:17 +0700

    complete connect to vue app

---
+++ resources/views/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  // Các tùy chọn component (data, methods, computed, etc.)
+}
+</script>
+
+<template>
+  <div>
+    <h1>Xin chào từ component Vue!</h1>
+  </div>
+</template>
+
+<style scoped>
+/* CSS dành riêng cho component này */
+</style>
