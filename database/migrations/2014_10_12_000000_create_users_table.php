e9fdb9e89716489e8d7732f196b5baebb9e23dea
Newt Louis <l.ngoc.17395@gmail.com>
2024-09-02 17:45:20 +0700

    create seed for users, add nullable for rememberToken()

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration {
             $table->string('password');
             $table->string('phone');
             $table->string('role');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
             $table->timestamps();
         });
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,18 +5,50 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
+    use WithoutModelEvents;
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
         DB::table('users')->insert([
+            [
             'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'email_verify_at' => Carbon::now(),
-        ]);
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('123456'),
+            'phone' => '0123456789',
+            'role' => '1',
+            ],
+            [
+            'name' => 'admin2',
+            'email' => 'admin2@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('123456'),
+            'phone' => '0123456789',
+            'role' => '2',
+            ],
+            [
+            'name' => 'admin3',
+            'email' => 'admin3@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('123456'),
+            'phone' => '0123456789',
+            'role' => '3',
+            ],
+            [
+            'name' => 'client',
+            'email' => 'client@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('123456'),
+            'phone' => '0123456789',
+            'role' => '4',
+            ]
+    ]);
     }
 }
