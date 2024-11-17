d3324dd52f88e2b3e0d81a254ed5d5fabb49d1b1
Newt Louis <l.ngoc.17395@gmail.com>
2024-11-17 17:15:22 +0700

    change list of products UI from table to list group <ul>

--- resources/views/Admin/Pages/ProductsComponent/AdminProducts.vue
+++ resources/views/Admin/Pages/ProductsComponent/AdminProducts.vue
@@ -46,86 +46,93 @@
       </div>
     </div>
     <div class="products-main mb-4">
-      <div class="table-responsive w-100">
-        <table class="table table-hover caption-top">
-          <caption>
-            List of Products
-          </caption>
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Images</th>
-              <th scope="col">Description</th>
-              <th scope="col">Gender</th>
-              <th scope="col">Origin</th>
-              <th scope="col">Category name</th>
-              <th scope="col">Capacities</th>
-              <th scope="col">Collection</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, index) in getProductsData" :key="index">
-              <td>
-                <span class="fw-bold">{{ item.id }}</span>
-              </td>
-              <td>
-                <a href="">{{ item.name }}</a>
-              </td>
-              <td>
-                <div class="images-container" v-if="item.product_images.length > 0">
+      <p class="fs-5 text-secondary">List of products</p>
+      <div class="product-lists">
+        <div class="lists-container">
+          <ul class="list-group list-group-flush">
+            <li
+              class="list-group-item list-group-item-action d-flex flex-column"
+              v-for="(item, index) in getProductsData"
+              :key="index"
+              data-bs-toggle="collapse"
+              :data-bs-target="`#${item.id + '_' + index}`"
+              aria-expanded="false"
+              :aria-controls="item.id + '_' + index"
+            >
+              <div class="row row-cols-auto w-100 row-gap-2">
+                <div class="col">
+                  <span class="fw-bold">ID: {{ item.id }}</span>
+                </div>
+                <div class="col">
+                  Name: <a href="">{{ item.name }}</a>
+                </div>
+                <div class="col d-flex me-2">
                   <img
-                    class="me-2"
-                    v-for="(image, index) in item.product_images"
-                    :key="index"
-                    :src="image.fullpath"
-                    alt=""
                     width="50px"
                     height="50px"
+                    v-for="image in item.product_images"
+                    :key="image.id"
+                    :src="image.fullpath"
+                    :alt="image.product_id"
                   />
                 </div>
-                <div v-else>
-                  <img src="/blank_avatar.jpg" alt="" width="50px" height="50px" />
-                </div>
-              </td>
-              <td class="text-wrap">{{ item.description }}</td>
-              <td>{{ item.gender }}</td>
-              <td>{{ item.origin }}</td>
-              <td>{{ item.item_type.name }}</td>
-              <td>
-                <div v-if="item.capacities.length > 0" class="d-inline-flex p-2 flex-wrap">
-                  <span
-                    class="badge text-wrap text-bg-secondary capacity-badge me-2"
-                    v-for="(capacity, index) in item.capacities"
-                    :key="index"
-                    >{{ capacity.name + " (" + capacity.volume + ")" }}</span
-                  >
-                </div>
-                <div v-else>
-                  <span>None</span>
-                </div>
-              </td>
-              <td>
-                <div v-if="item.capacities.length > 0">
-                  <div v-for="(capacity, index) in item.capacities" :key="index">
-                    <div v-if="capacity.pivot.collections.length > 0">
-                      <span class="badge text-bg-secondary">{{ capacity.name }}</span>
-                      <span class="me-2" v-for="(collect, index) in capacity.pivot.collections" :key="index">{{
-                        collect.name
-                      }}</span>
-                    </div>
-                    <div v-else>
-                      <span>None</span>
-                    </div>
+                <div class="col">Description: {{ item.description }}</div>
+                <div class="col">Gender: {{ item.gender }}</div>
+                <div class="col">Origin: {{ item.origin }}</div>
+                <div class="col">Category name: {{ item.item_type.name }}</div>
+              </div>
+              <div class="collapse" :id="item.id + '_' + index" v-if="item.capacities.length > 0">
+                <hr />
+                <div class="row row-cols-auto w-100" v-for="(capacity, index) in item.capacities" :key="index">
+                  <div class="col">
+                    <span
+                      >Capacity:
+                      <span class="badge text-bg-primary">{{
+                        capacity.name + " (" + capacity.volume + ")"
+                      }}</span></span
+                    >
+                  </div>
+                  <div class="col" v-if="capacity.pivot.collections.length > 0">
+                    <span
+                      >Collection:
+                      <span
+                        class="badge text-bg-secondary me-2"
+                        v-for="(collect, index) in capacity.pivot.collections"
+                        :key="index"
+                        >{{ collect.name }}</span
+                      ></span
+                    >
+                  </div>
+                  <div class="col" v-else>
+                    <span>Collection: None</span>
+                  </div>
+                  <div class="col">
+                    <span v-if="capacity.pivot.inventory !== null"
+                      >Inventory: <span class="badge text-bg-success">{{ capacity.pivot.inventory }}</span>
+                    </span>
+                    <span v-else>Inventory: None</span>
+                  </div>
+                  <div class="col">
+                    <span v-if="capacity.pivot.supplier !== null">Supplier: {{ capacity.pivot.supplier }}</span>
+                    <span v-else>Supplier: None</span>
+                  </div>
+                  <div class="col">
+                    <span v-if="capacity.pivot.price !== null">Price: {{ capacity.pivot.price }}</span>
+                    <span v-else>Price: None</span>
+                  </div>
+                  <div class="col">
+                    <span v-if="capacity.pivot.discount !== null">Discount: {{ capacity.pivot.discount }}</span>
+                    <span v-else>Discount: None</span>
                   </div>
                 </div>
-                <div v-else>
-                  <span>None</span>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </div>
+              <div class="collapse" :id="item.id + '_' + index" v-else>
+                <hr />
+                <span class="fw-bold fs-5">None</span>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <div class="pagination-container float-end">
@@ -243,6 +250,16 @@ tbody > tr > td {
 }
 tbody > tr > td:first-child {
   width: 50px;
+}
+.product-lists {
+  width: 100%;
+  overflow-x: scroll;
+}
+.lists-container {
+  width: 200%;
+}
+.list-group-item:hover {
+  cursor: pointer;
 }
 .capacity-badge {
 }
